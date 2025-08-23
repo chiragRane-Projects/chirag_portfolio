cdb161227edbd3b227ecc0d192cc76fbb5f9e584
Chirag Rane <beingchirag7@gmail.com>
2025-08-23 15:52:29 +0530

    Navbar Completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,50 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
 
 const Navbar = () => {
+  const navLinks = [
+    { href: "#about", label: "About Me" },
+    { href: "#projects", label: "Projects" },
+    { href: "#techstack", label: "Techstack" },
+    { href: "#contact", label: "Conatct Me"}
+  ];
   return (
-    <header className='flex flex-row p-5'>
-        <div>
-          <h2>
+    <header className='flex flex-row p-5 md:p-8 items-center justify-between'>
+          <Link href="/">
+          <h2 className='font-semibold text-2xl'>
             Chirag Rane
           </h2>
-        </div>
+          </Link>
+
+          <nav className="hidden md:block">
+          <ul className="flex flex-row items-center gap-6 text-black font-medium">
+            {navLinks.map(({ href, label }) => (
+              <Link key={href} href={href}>
+                <li
+                  className="relative transition-all duration-300
+                  after:content-[''] after:absolute after:left-0 after:bottom-0
+                  after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300
+                  hover:after:w-full cursor-pointer"
+                >
+                  {label}
+                </li>
+              </Link>
+            ))}
+            <Link href="https://calendly.com/beingchirag6/30min">
+              <Button className="bg-transparent border-2 text-black border-zinc-800 hover:bg-transparent cursor-pointer rounded-sm">
+                <p className="text-md">Download Resume</p>
+              </Button>
+            </Link>
+          </ul>
+        </nav>
+
+        <main className='md:hidden'>
+          <Button className="border-2 border-zinc-800 bg-transparent rounded-sm">
+            <p className="text-md text-black ">Download Resume</p>
+          </Button>
+        </main>
     </header>
   )
 }
