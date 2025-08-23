10346ab857ecc8dd7349d831cc022b22d4a68a67
Chirag Rane <beingchirag7@gmail.com>
2025-08-23 15:39:15 +0530

    Navbar begins

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='flex flex-row p-5'>
+        <div>
+          <h2>
+            Chirag Rane
+          </h2>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
