cb7d8512f0e2b5f3884b168431a1e186d3e97fe9
Chirag Rane <beingchirag7@gmail.com>
2025-09-21 03:12:16 +0530

    New Project added

--- projects.js
+++ projects.js
@@ -1,5 +1,12 @@
 export const ProjectsData = [
     {
+        "Name": "Aarunya Studios Website",
+        "Description": "The official landing page for Aarunya Studios, my own tech and marketing agency. I co-founded the agency and led the design and development of this site, showcasing our services and brand identity. Built as a client-ready, production-grade website, it emphasizes clean design, smooth interactions, and responsive performance.",
+        "Techstack": "NextJS 15 App Router, Tailwind CSS, ShadcnUI",
+        "GithubURL": null,
+        "LiveURL": "https://aarunyastudios.co.in/"
+    },
+    {
         Name: "FoodTrient",
         Description: "Foodtrient is an AI based project, which detects your food and gives you its overall nutrition information. It also works as a nutritionist chatbot.",
         Techstack: "NextJS, TailwindCSS, ShadcnUI, GeminiAI",
@@ -43,16 +50,16 @@ export const ProjectsData = [
     },
     {
         "Name": "Restaurant Mobile App",
-        "Description":"Customer side facing mobile application where customers can order their favourite dish. To try out the application kindly clone the repository.",
+        "Description": "Customer side facing mobile application where customers can order their favourite dish. To try out the application kindly clone the repository.",
         "Techstack": "React Native, Expo",
         "GithubURL": "https://github.com/chiragRane-Projects/restaurant_mobile_app.git",
         "LiveURL": null
     },
     {
-        "Name":"Aarunya Studios Website",
-        "Description": "Landing page for Aarunya Studios. Aarunya Studios is a tech and marketing agency offering a variety of services. Since its a client project the github repository cannot be shared",
-        "Techstack":"NextJS 15 App Router, Tailwind CSS, ShadcnUI",
-        "GithubURL":null,
-        "LiveURL":"https://aarunyastudios.co.in/"
+        "Name": "Appniche Technologies Website",
+        "Description": "Appniche Technologies is a tech startup which offers variety of IOT & software products which helps the business owners operate their functions easily. Its a client work and hence the github repository cannot be shared.",
+        "Techstack": "NextJS 15 App Router, Tailwind CSS, ShadcnUI, Framer-Motion",
+        "GithubURL": null,
+        "LiveURL": "https://appniche-technologies.vercel.app/"
     }
 ]
